745be5fe107f3a8a25a256d1d03a5eae2b2f10b3
SwiftlyAside <ivecis@live.co.kr>
2021-11-11 17:57:09 +0900

    Add theme config

---
+++ lib/theme.ts
@@ -0,0 +1,48 @@
+import { mode, StyleFunctionProps } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react'
+
+const styles = {
+  global: (props: StyleFunctionProps) => ({
+    body: {
+      bg: mode('#dbeef0', '#202023')(props),
+    },
+  }),
+}
+
+const components = {
+  Heading: {
+    variants: {
+      'section-title': {
+        textDecoration: 'underline',
+        fontSize: 20,
+        textUnderlineOffset: 6,
+        textDecorationColor: '#525252',
+        textDecorationThickness: 4,
+        marginTop: 3,
+        marginBottom: 4,
+      },
+    },
+  },
+  Link: {
+    baseStyle: (props: StyleFunctionProps) => ({
+      color: mode('#3d7aed', '#ff63c3')(props),
+      textUnderlineOffset: 3,
+    }),
+  },
+}
+
+const fonts = {
+  heading: "'Gothic A1'",
+}
+
+const colors = {
+  grassTeal: '#88ccca',
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+const theme = extendTheme({ config, styles, components, fonts, colors })
+export default theme
