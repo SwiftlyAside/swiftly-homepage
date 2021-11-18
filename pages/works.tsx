39773d14dcd56d00babbaf236c798c30b44acf6a
SwiftlyAside <ivecis@live.co.kr>
2021-11-18 17:56:48 +0900

    Add article, works

---
+++ pages/works.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/layouts/article'
+import { Container, Heading } from '@chakra-ui/react'
+
+const Works = () => (
+  <Layout title="Works">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+    </Container>
+  </Layout>
+)
+
+export default Works
